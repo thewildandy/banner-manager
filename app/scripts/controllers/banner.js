68ef06e318bddbd24e732748aa8ed459eb9f04ae
Andy Wild <thewildandy@gmail.com>
2014-02-20 14:00:45 +0000

    Implemented add / edit banner modal

--- app/scripts/controllers/banner.js
+++ app/scripts/controllers/banner.js
@@ -1,42 +1,86 @@
 'use strict';
 
 angular.module('bannerManagerApp')
-  .controller('BannerCtrl', function ($scope, $modal, BannerService, $firebase) {
+  .controller('BannerCtrl', function ($scope, $modal, BannerService) {
     $scope.banners = BannerService.getBanners();
 
     $scope.create = function() {
-        var banner = {
-            id: -1,
-            name: '',
-            images: []
-        };
+      var banner = {
+        name: '',
+        file: '',
+        images: {}
+      };
 
+      var modal = $modal.open({
+        templateUrl: 'views/banner-modal.html',
+        controller: 'BannerModalCtrl',
+        resolve: {
+          banner: function() {
+            return banner;
+          },
+          identifier: function() {
+            return -1;
+          }
+        }
+      });
+    }
+
+    $scope.edit = function(identifier) {
+      BannerService.getBanner(identifier).then(function(banner) {
         var modal = $modal.open({
             templateUrl: 'views/banner-modal.html',
             controller: 'BannerModalCtrl',
             resolve: {
-                banner: function() {
-                    return banner;
-                }
+              banner: function() {
+                  return banner;
+              },
+              identifier: function() {
+                return identifier;
+              }
             }
         });
+      });
     }
 
-    $scope.edit = function(index) {
-        BannerService.getBanner(index).then(function(banner) {
-            var modal = $modal.open({
-                templateUrl: 'views/banner-modal.html',
-                controller: 'BannerModalCtrl',
-                resolve: {
-                    banner: function() {
-                        return banner;
-                    }
-                }
-            });
-        });
-    }
+    $scope.$on('bannerDeleted', function(event, identifier) {
+      delete $scope.banners[identifier];
+    });
   })
 
-  .controller('BannerModalCtrl', function($scope, $modalInstance, banner) {
+  .controller('BannerModalCtrl', function ($scope, $modalInstance, ImageService, BannerService, banner, identifier) {
     $scope.banner = banner;
+    $scope.identifier = identifier;
+
+    ImageService.getImages().then(function(images) {
+      $scope.availableImages = images;
+    });
+
+    $scope.addImage = function(key) {
+      if($scope.banner.images == undefined)
+        $scope.banner.images = {};
+
+      $scope.banner.images[key] = $scope.availableImages[key];
+    }
+
+    $scope.removeImage = function(key) {
+      delete $scope.banner.images[key];
+    }
+
+    $scope.save = function() {
+      if(identifier == -1) {
+        BannerService.createBanner($scope.banner).then(function () {
+          $modalInstance.close();
+        });  
+      } else {
+        BannerService.updateBanner(identifier, $scope.banner).then(function () {
+          $modalInstance.close();
+        });
+      }
+    }
+
+    $scope.delete = function() {
+      BannerService.deleteBanner(identifier).then(function () {
+        $modalInstance.close();
+      });
+    }
   });
--- app/scripts/services/banner.js
+++ app/scripts/services/banner.js
@@ -1,20 +1,32 @@
 'use strict';
 
 angular.module('bannerManagerApp')
-  .factory('BannerService', function ($firebase, $http) {
+  .factory('BannerService', function ($rootScope, $firebase, $http) {
   	var firebaseUrl = 'https://twa.firebaseio.com/banner-manager';
   	var banners = new Firebase(firebaseUrl + '/banners');
     return {
+      createBanner: function(data) {
+        return $http.post(firebaseUrl + '/banners.json', data).then(function(response) {
+          return response.data;
+        });
+      },
     	getBanners: function() {
     		return $firebase(banners);
     	},
-    	getBanner: function(index) {
-    		var ref = $firebase(banners);
-    		var keys = ref.$getIndex();
-
-    		return $http.get(firebaseUrl + '/banners/' + keys[index] + '.json').then(function(response) {
+    	getBanner: function(identifier) {
+    		return $http.get(firebaseUrl + '/banners/' + identifier + '.json').then(function(response) {
     			return response.data;
     		});
-    	}
+    	},
+      updateBanner: function(identifier, data) {
+        return $http.put(firebaseUrl + '/banners/' + identifier + '.json', data).then(function(response) {
+          $rootScope.$broadcast('bannerUpdated', identifier, response.data);
+        });
+      },
+      deleteBanner: function(identifier) {
+        return $http.delete(firebaseUrl + '/banners/' + identifier + '.json').then(function (response) {
+          $rootScope.$broadcast('bannerDeleted', identifier);
+        });
+      }
     };
   });
