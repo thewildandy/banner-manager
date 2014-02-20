27c7256b54ca2f28a36315a9f6d208cb34bfff3d
Andy Wild <thewildandy@gmail.com>
2014-02-20 12:50:18 +0000

    Implemented modal with edit and delete functionality for images

--- app/scripts/controllers/image.js
+++ app/scripts/controllers/image.js
@@ -1,12 +1,49 @@
 'use strict';
 
 angular.module('bannerManagerApp')
-  .controller('ImageCtrl', function ($scope, ImageService) {
-    $scope.images = ImageService.getImages();
+  .controller('ImageCtrl', function ($scope, $modal, ImageService) {
+    ImageService.getImages().then(function(images) {
+      $scope.images = images;
+    });
 
-    $scope.edit = function(index) {
-    	ImageService.getImage(index).then(function(image) {
-    		console.log(image);
-    	});
+    $scope.$on('imageUpdated', function (event, identifier, image) {
+      $scope.images[identifier] = image;
+    })
+
+    $scope.$on('imageDeleted', function (event, identifier) {
+      delete $scope.images[identifier];
+    });
+
+    $scope.edit = function(identifier) {
+      ImageService.getImage(identifier).then(function(image) {
+        $modal.open({
+          templateUrl: 'views/image-modal.html',
+          controller: 'ImageModalCtrl',
+          resolve: {
+            image: function() {
+              return image;
+            },
+            identifier: function() {
+              return identifier;
+            }
+          }
+        });
+      });
+    }
+  })
+
+  .controller('ImageModalCtrl', function ($scope, $rootScope, $modalInstance, ImageService, image, identifier) {
+    $scope.image = image;
+
+    $scope.save = function() {
+      ImageService.updateImage(identifier, $scope.image).then(function(image) {
+        $modalInstance.close();
+      });
+    }
+
+    $scope.delete = function() {
+      ImageService.deleteImage(identifier).then(function() {
+        $modalInstance.close();
+      });
     }
   });
--- app/scripts/services/image.js
+++ app/scripts/services/image.js
@@ -1,20 +1,28 @@
 'use strict';
 
 angular.module('bannerManagerApp')
-  .factory('ImageService', function ($firebase, $http) {
+  .factory('ImageService', function ($rootScope, $firebase, $http) {
   	var firebaseUrl = 'https://twa.firebaseio.com/banner-manager';
   	var images = new Firebase(firebaseUrl + '/images');
     return {
     	getImages: function() {
-    		return $firebase(images);
-    	},
-      getImage: function(index) {
-        console.log(index);
-        var ref = $firebase(images);
-        var keys = ref.$getIndex();
-
-        return $http.get(firebaseUrl + '/images/' + keys[index] + '.json').then(function(response) {
+    		return $http.get(firebaseUrl + '/images.json').then(function(response) {
           return response.data;
+        });
+    	},
+      getImage: function(identifier) {
+        return $http.get(firebaseUrl + '/images/' + identifier + '.json').then(function(response) {
+          return response.data;
+        });
+      },
+      updateImage: function(identifier, data) {
+        return $http.put(firebaseUrl + '/images/' + identifier + '.json', data).then(function(response) {
+          $rootScope.$broadcast('imageUpdated', identifier, response.data);
+        });
+      },
+      deleteImage: function(identifier) {
+        return $http.delete(firebaseUrl + '/images/' + identifier + '.json').then(function(response) {
+          $rootScope.$broadcast('imageDeleted', identifier);
         });
       }
     };
